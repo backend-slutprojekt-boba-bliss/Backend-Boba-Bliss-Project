ee752b319d1925dbff7e1412900bb5f39d7e37ca
Linus Hammarberg <Linus.hammarberg@gmail.com>
2023-05-19 13:11:41 +0200

    Created productschema and model
    Co-authored-by: Hanna Petersson <hanna.petersson@medieinstitutet.se>

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,7 +3,6 @@ import express, {
   Request,
   Response,
 } from "express";
-import "express-async-errors";
 
 export const app = express();
 app.use(express.json());
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,54 @@
 import mongoose from "mongoose";
-import { app } from "./app"
+import { app } from "./app";
 
 async function main() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://BobaTea:BobaTea@slutprojekt-backend.0lfqeuo.mongodb.net/`
+    );
+    console.log("Connected to Database");
 
-  await mongoose.connect(
-    `mongodb+srv://BobaTea:BobaTea@slutprojekt-backend.0lfqeuo.mongodb.net/`
-  );
-  console.log("Connected to Database");
+    app.listen(3000, () => {
+      console.log("Server is running: http://localhost:3000");
 
-  app.listen(3000, () => {
-    console.log("Server is running: http://localhost:3000");
-  });
+      createProducts();
+    });
+  } catch (error) {
+    console.error("Error starting server:", error);
+  }
+}
+
+async function createProducts() {
+  try {
+    const productSchema = new mongoose.Schema({
+      imageUpload: String,
+      imageAlt: String,
+      title: String,
+      description: String,
+      price: Number,
+      backgroundColour: String,
+      quantity: Number,
+      inStock: Number,
+    });
+
+    // Create the Product model using the product schema
+    const Product = mongoose.model("Product", productSchema);
+
+    // Create an array of products
+    const products = [
+      { title: "Product 1", price: 10, imageUpload: "Hejhejhej", imageAlt: "HejhejDÅ", description: "Detta är världens godaste Bobate", backgroundColour: "Väldigt grön", quantity: 4000, inStock: 2 },
+    ];
+
+    // Create documents for each product and save them to the database
+    const createdProducts = await Product.create(products);
+
+    console.log("Products created successfully:", createdProducts);
+  } catch (error) {
+    console.error("Error creating products:", error);
+  } finally {
+    // Disconnect from the database
+    mongoose.disconnect();
+  }
 }
 
 main().catch(console.error);
