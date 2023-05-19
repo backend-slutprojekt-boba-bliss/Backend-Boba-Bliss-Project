7c244a7d753cb206530994531505d2807dabf3d7
Linus Hernvall <linus.hernvall@gmail.com>
2023-05-19 15:01:32 +0200

    Get products

--- server/src/resources/products/productControllers.ts
+++ server/src/resources/products/productControllers.ts
@@ -1,8 +1,15 @@
 import { Request, Response } from "express";
 import { ProductModel } from "./productModel";
+import "express-async-errors";
 
 export async function getAllProducts(req: Request, res: Response) {
   console.log("get all products");
+  try {
+    const products = await ProductModel.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching products" });
+  }
 }
 
 export async function createProduct(req: Request, res: Response) {
